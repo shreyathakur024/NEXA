bc1ead30da8b027f4b2812b2d6054e4f4600a078
shreyathakur024 <shreyathakur200402@gmail.com>
2026-07-08 15:32:28 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // KB
+  },
   plugins: [
     react(),
     nodePolyfills(), // Ye library ab automatically 'events' aur 'util' handle kar legi
