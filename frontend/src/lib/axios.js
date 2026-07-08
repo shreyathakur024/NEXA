69782a04d7b96c77e781b5c2df024d983bbce543
shreyathakur024 <shreyathakur200402@gmail.com>
2026-07-08 12:49:43 +0530

    cors url and routes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseUrl = import.meta.env.VITE_API_URL || "http://localhost:5004";
+const apiBaseUrl = import.meta.env.VITE_API_URL || "https://nexa-hj2s.onrender.com/api";
 
 export const axiosInstance = axios.create({
     baseURL: apiBaseUrl,
